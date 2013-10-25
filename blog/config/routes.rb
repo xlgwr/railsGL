b8b5527a69b9ff42856755ea1345ef16f044eca5
xlgwr <ubuntu>
2013-10-25 23:50:16 +0800

    add comment

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,5 +1,7 @@
 Blog::Application.routes.draw do
-  resources :posts
+  resources :posts do
+    resource :comments
+  end
   get "welcome/index"
 
   # The priority is based upon order of creation: first created -> highest priority.
