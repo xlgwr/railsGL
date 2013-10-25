6a1c86f79c51c6850d152a0f806488e326efd1b8
xlgwr <ubuntu>
2013-10-25 21:37:41 +0800

    add form delete

--- blog/app/controllers/posts_controller.rb
+++ blog/app/controllers/posts_controller.rb
@@ -29,6 +29,12 @@ class PostsController < ApplicationController
       render 'edit'
     end
   end
+  def destroy
+    @post=Post.find(params[:id])
+    @post.destroy
+
+    redirect_to posts_path
+  end
   private
   def post_params
     params.require(:post).permit(:title,:text)
