41ca7a1b04dc436547362ee8727093262f7dd296
xlgwr <xlgwr@163.com>
2013-10-25 16:58:29 +0800

    add blog

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '9f2de63318334f7c31df9e70d1a77351c01deb7d368cd5899abe46c017cd85c1868230d0568a8958f3154853bbfcb168debe00ef93a53b5377b37340953b71bf'
