3c27ab908e20ea78c058045c8ab2f115b62b50a4
xlgwr <xlgwr@163.com>
2013-10-25 18:19:47 +0800

    add CURD for blog

--- blog/app/controllers/posts_controller.rb
+++ blog/app/controllers/posts_controller.rb
@@ -18,6 +18,17 @@ class PostsController < ApplicationController
       render 'new'
     end
   end
+  def edit
+    @post = Post.find(params[:id])
+  end
+  def update
+    @post=Post.find(params[:id])
+    if @post.update(params[:post].permit(:title, :text))
+      redirect_to @post
+     else
+      render 'edit'
+    end
+  end
   private
   def post_params
     params.require(:post).permit(:title,:text)
